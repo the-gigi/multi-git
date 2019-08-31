02cd3f4cf21acb8ce7f7d24154f560994c3d0027
Gigi <gigi.sayfan@helix.com>
2019-08-30 17:38:33 -0700

    refactor core logic to repo_manager package

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"log"
 	"os"
 	"strings"
-	"os/exec"
+
+	"github.com/the-gigi/multi-git/pkg/repo_manager"
 )
 
 func main() {
@@ -19,46 +20,21 @@ func main() {
 
 	// Get managed repos from environment variables
 	root := os.Getenv("MG_ROOT")
-	if root[len(root) - 1] != '/' {
+	if root[len(root)-1] != '/' {
 		root += "/"
 	}
 
-	repo_names := strings.Split(os.Getenv("MG_REPOS"), ",")
-	var repos []string
-	// Verify all repos exist and are actually git repos (have .git sub-dir)
-	for _, r := range repo_names {
-		path := root + r
-		_, err := os.Stat(path + "/.git")
-		if err != nil {
-			log.Fatal(err)
-		}
-		repos = append(repos, path)
+	repoNames := strings.Split(os.Getenv("MG_REPOS"), ",")
+
+	repoManager, err := repo_manager.NewRepoManager(root, repoNames, *ignoreErros)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Break the git command into components (needed to execute)
-	var git_components []string
-	for _, component := range strings.Split(*command, " ") {
-		git_components = append(git_components, component)
-	}
-	command_string := "git " + *command
-
-	for _, r := range repos {
-		// Go to the repo's directory
-		os.Chdir(r);
-
-		// Print the command
-		fmt.Printf("[%s] %s\n", r, command_string)
-
-		// Execute the command
-		out, err := exec.Command("git", git_components...).CombinedOutput()
-
-		// Print the result
-		fmt.Println(string(out))
-
-		// Bail out if there was an error and NOT ignoring errors
-		if err != nil && !*ignoreErros {
-			os.Exit(1)
-		}
+	output, _ := repoManager.Exec(*command)
+	for repo, out := range output {
+		fmt.Printf("[%s]: %s\n", repo, command)
+		fmt.Println(out)
 	}
 
 	fmt.Println("Done.")
---
+++ pkg/repo_manager/repo_manager.go
@@ -0,0 +1,82 @@
+package repo_manager
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type RepoManager struct {
+	repos        []string
+	ignoreErrors bool
+}
+
+func NewRepoManager(baseDir string, repoNames []string, ignoreErrors bool) (repoManager *RepoManager, err error) {
+	_, err = os.Stat(baseDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = errors.New(fmt.Sprintf("base dir: '%s' doesn't exist", baseDir))
+		}
+		return
+	}
+
+	if baseDir[len(baseDir)-1] != '/' {
+		baseDir += "/"
+	}
+
+	if len(repoNames) == 0 {
+		err = errors.New("repo list can't be empty")
+		return
+	}
+
+	repoManager = &RepoManager{
+		ignoreErrors: ignoreErrors,
+	}
+	for _, r := range repoNames {
+		path := baseDir + r
+		_, err := os.Stat(path + "/.git")
+		if err != nil {
+			if os.IsNotExist(err) {
+				err = errors.New(fmt.Sprintf("directory '%s' is not a git repo", path))
+			}
+			return
+		}
+		repoManager.repos = append(repoManager.repos, path)
+	}
+
+	return
+}
+
+func (m *RepoManager) GetRepos() []string {
+	return m.repos
+}
+
+func (m *RepoManager) Exec(cmd string) (output map[string]string, err error) {
+	output = map[string]string{}
+	var components []string
+	for _, component := range strings.Split(cmd, " ") {
+		components = append(components, component)
+	}
+
+	// Restore working directory after executing the command
+	wd, _ := os.Getwd()
+	defer os.Chdir(wd)
+
+	for _, r := range m.repos {
+		// Go to the repo's directory
+		os.Chdir(r);
+
+		// Execute the command
+		out, err := exec.Command("git", components...).CombinedOutput()
+		// Store the result
+		output[r] = string(out)
+
+		// Bail out if there was an error and NOT ignoring errors
+		if err != nil && !m.ignoreErrors {
+			return
+		}
+	}
+	return
+}
