172b80163ab9a87aa791b42fa8e94e1bbd7686f6
the-gigi <the.gigi@gmail.com>
2020-07-03 22:30:15 -0700

    remove embedded info

--- main.go
+++ main.go
@@ -10,23 +10,19 @@ import (
 	"github.com/the-gigi/multi-git/cmd"
 )
 
-var (
-	GitTag    string
-	Timestamp string
+const (
+	version = "v0.8.18"
 )
 
 func main() {
-	if GitTag != "" {
-		fmt.Printf("Git tag : %s\nBuilt at: %s\n\n", GitTag, Timestamp)
-
-		v := semver.MustParse(GitTag[1:])
-		latest, err := selfupdate.UpdateSelf(v, "the-gigi/multi-git")
-		if err != nil {
-			log.Fatalf("Binary update failed: %v", err)
-			return
-		} else {
-			fmt.Println("Current version is:", latest.Version)
-		}
+	fmt.Println("version: ", version)
+	v := semver.MustParse(version[1:])
+	latest, err := selfupdate.UpdateSelf(v, "the-gigi/multi-git")
+	if err != nil {
+		log.Fatalf("Binary update failed: %v", err)
+		return
+	} else {
+		fmt.Println("Current version is:", latest.Version)
 	}
 
 	cmd.Execute()
